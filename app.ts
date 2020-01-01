adebc1b9b3733fe3e739b1fa18a1e5d4d0324e26
KD0NKS <andrew.m.fairhurst@gmail.com>
2020-01-01 13:47:53 -0600

    * Fixed issue with sending erroneous login packets to server * Only 1 parser is created instead of per packet. * Reduced output to console.

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import ISSocket from 'js-aprs-is';
+import { ISSocket} from 'js-aprs-is';
 import { aprsParser } from 'js-aprs-fap';
 
 const CALLSIGN = 'NOCALL';
@@ -8,18 +8,19 @@ const PORTNUMBER = 14580;
 const FILTER = 'r/39.00/-91.00/1000';
 
 let connection = new ISSocket(APRSSERVER, PORTNUMBER, CALLSIGN, PASSCODE, FILTER);
+let parser = new aprsParser();
+
 connection.connect();
 
-connection.on('packet', (data: string) => {
-    let parser = new aprsParser();
-    
-    if(data.charAt(0) == '#') {
-        connection.sendLine(connection.userLogin)
-    } else {
-        let msg = parser.parseaprs(data);
+connection.on('connect', () => {
+    connection.sendLine(connection.userLogin);
+});
 
+connection.on('packet', (data: string) => {
+    if(data.charAt(0) != '#' && !data.startsWith('user')) {
+        console.log(parser.parseaprs(data));
+    } else {
         console.log(data);
-        console.log(msg);
     }
 });
 
