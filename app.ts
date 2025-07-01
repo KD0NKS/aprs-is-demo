4042497d062063722717b5b776f9d7159b06538e
KD0NKS <KD0NKS@users.noreply.github.com>
2025-06-30 20:36:19 -0500

    2025 07 updates (#18)
    - Updating dependencies
    - Refactor to account for update changes to js-aprs-is library
    - Adding package lock

--- app.ts
+++ app.ts
@@ -1,24 +1,38 @@
 import { ISSocket } from 'js-aprs-is';
-import { aprsParser } from 'js-aprs-fap';
+import { aprsParser, PacketTypeEnum } from 'js-aprs-fap';
 
-const CALLSIGN = 'NOCALL';
+const APPID = 'aprs-is-demo v2'
+const CALLSIGN = 'N0CALL';
 const PASSCODE = -1;
 const APRSSERVER = 'rotate.aprs2.net';
 const PORTNUMBER = 14580;
 const FILTER = 'r/39.00/-91.00/1000';
 
-let connection = new ISSocket(APRSSERVER, PORTNUMBER, CALLSIGN, PASSCODE, FILTER);
 let parser = new aprsParser();
+let connection = new ISSocket(
+    APPID
+    , APRSSERVER
+    , PORTNUMBER
+    , CALLSIGN
+    , PASSCODE
+    , false
+    , "TestConnection"
+    , FILTER
+    );
 
 connection.connect();
 
 connection.on('connect', () => {
-    connection.sendLine(connection.userLogin);
+    connection.sendLogin();
 });
 
 connection.on('packet', (data: string) => {
     if(data.charAt(0) != '#' && !data.startsWith('user')) {
-        console.log(parser.parseaprs(data));
+        const packet = parser.parseaprs(data);
+
+        // Any filtering/processing/formatting logic here.
+
+        console.log(packet);
     } else {
         console.log(data);
     }
